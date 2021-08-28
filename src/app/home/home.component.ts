fa1527947903e20c6c1c884aa2214ea3680fa96c
girish <reguntagirish@gmail.com>
2021-08-28 19:34:36 +0530

    spinner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,59 +13,59 @@ export class HomeComponent implements OnInit {
     private spinner: NgxSpinnerService) { }
   imageList=[
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"LONAVALA"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"PUNE"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"NAINITAL"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"MAHABALESHWAR"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"JAIPUR"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"SHIMLA"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"GOA"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"DELHI"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"BANGALORE"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"UDAIPUR"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"ALIBAUG"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"OOTY"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"COORG"
     },
     {
-      img:"../../assets/icons/Lorem-ipsum.png",
+      img:"assets/icons/Lorem-ipsum.png",
       name:"LAVASA"
     }
   ]
